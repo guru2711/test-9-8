e9fec7ce7afc937788d6968bdb1714c156776bbb
guru2711 <saiguruprakash@gmail.com>
2021-08-11 10:28:16 +0530

    gi

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
-const img = document.createElement("img")
-img.setAttribute("src","https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png")
-img.setAttribute("class","img")
+const img = document.createElement("img");
+img.setAttribute(
+  "src",
+  "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png"
+);
+img.setAttribute("class", "img");
 // document.body.append(img)
-const population = 88000070
-function country(){
-    const a = document.createElement("div")
-a.innerHTML = `<img class="img" src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png">
+const population = 88000070;
+function country() {
+  const a = document.createElement("div");
+  a.innerHTML = `<div class="row">
+  <div class="germany">
+  <img class="img" src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png">
 <h1>Germany</h1>
 <p><b>Population</b> : 
 81,775,900</p>
 <p><b>Region</b> : Europe</p>
 <p><b>Capital</b> :Berlin</p>
+</div>
+<div class="india">
 <img class="img" src="https://upload.wikimedia.org/wikipedia/en/4/41/Flag_of_India.svg">
 <h1>India</h1>
 <p><b>Population</b> : 
 1,380,004,385</p>
 <p><b>Region</b> : Asia</p>
 <p><b>Capital</b> : Dehli</p>
+</div>
+<div class="usa">
 <img class="img" src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg">
 <h1>United States of America</h1>
 <p><b>Population</b> : 
 1,380,004,22</p>
 <p><b>Region</b> : Americas</p>
 <p><b>Capital</b> : Washington.D.C</p>
+</div>
+<div class="korea">
 <img class="img" src="https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg">
 <h1>South Korea</h1>
 <p><b>Population</b> : 
 5,17,10,000</p>
 <p><b>Region</b> : Asia</p>
 <p><b>Capital</b> : Seoul</p>
-`
-document.body.append(a)
+</div>
+</div>
+`;
+  document.body.append(a);
 }
-country()
+country();
 // for(var i = 0; i < 5; i++ ){
 //     country()
 // }
-
